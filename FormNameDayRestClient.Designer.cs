1637c4eb86726d49a41a167c5320cf27edab09f2
Petr Faltus <petrfaltus@petrfaltus.net>
2020-03-10 15:56:39 +0100

    Implement the menu with the About information

--- FormNameDayRestClient.Designer.cs
+++ FormNameDayRestClient.Designer.cs
@@ -1,8 +1,14 @@
+using System;
+using System.Windows.Forms;
 
 namespace NameDayRestClient
 {
     public partial class FormNameDayRestClient
     {
+        private MainMenu mainMenu;
+        private MenuItem menuItemInfo;
+        private MenuItem menuItemAbout;
+
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -20,6 +26,22 @@ namespace NameDayRestClient
             base.Dispose(disposing);
         }
 
+        private MainMenu InitializeMenu()
+        {
+            // Info menu items
+            this.menuItemAbout = new MenuItem("&About");
+            this.menuItemAbout.Click += new EventHandler(this.menuItemAboutClick);
+
+            this.menuItemInfo = new MenuItem("&Info");
+            this.menuItemInfo.MenuItems.Add(this.menuItemAbout);
+
+            // final menu bar
+            this.mainMenu = new MainMenu();
+            this.mainMenu.MenuItems.Add(this.menuItemInfo);
+
+            return mainMenu;
+        }
+
         /// <summary>
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
@@ -32,6 +54,8 @@ namespace NameDayRestClient
             this.SuspendLayout();
             this.Name = "NameDayRestClient";
             this.Text = "Name day REST client";
+
+            this.Menu = InitializeMenu();
 
             this.ResumeLayout(false);
             this.PerformLayout();
--- FormNameDayRestClient.cs
+++ FormNameDayRestClient.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms;
 
 namespace NameDayRestClient
@@ -7,6 +8,32 @@ namespace NameDayRestClient
         public FormNameDayRestClient()
         {
             InitializeComponent();
+        }
+
+        private void aboutApplication()
+        {
+            Version dotNetVer = Environment.Version;
+            OperatingSystem os = Environment.OSVersion;
+
+            string message = "Author: Petr Faltus © March 2020";
+            message += Environment.NewLine;
+            message += Environment.NewLine;
+
+            message += ".NET version: " + dotNetVer;
+            message += Environment.NewLine;
+            message += "Operating system: " + os.VersionString;
+            message += Environment.NewLine;
+
+            message += " ";
+
+            string title = "About the " + this.Text;
+
+            MessageBox.Show(message, title);
+        }
+
+        private void menuItemAboutClick(object sender, EventArgs e)
+        {
+            aboutApplication();
         }
     }
 }
